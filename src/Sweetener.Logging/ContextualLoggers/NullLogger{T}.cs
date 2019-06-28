3ff15f390923579197685dc3a41cffa64deea7d7
Will Sugarman <will.sugarman@microsoft.com>
2019-06-28 12:33:51 -0700

    Minor formatting

--- src/Sweetener.Logging/ContextualLoggers/NullLogger{T}.cs
+++ src/Sweetener.Logging/ContextualLoggers/NullLogger{T}.cs
@@ -4,16 +4,16 @@
     {
         public override bool IsSynchronized => true;
 
-        protected internal override void Log(LogEntry<T> logEntry)
-        { }
-
         protected override void Dispose(bool disposing)
         {
             // There is nothing to dispose and we don't want users to be able to
             // dispose of a logger used statically!
             //
             // Furthermore, repeated calls to GC.SuppressFinalize(this) are just fine
-            // in Logger.Dispose() since there is no finalizer
+            // in Logger<T>.Dispose() since there is no finalizer
         }
+
+        protected internal override void Log(LogEntry<T> logEntry)
+        { }
     }
 }
