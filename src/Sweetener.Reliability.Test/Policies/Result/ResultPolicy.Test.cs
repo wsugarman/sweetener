8db6c756bbffd23156b467ba92d02590dd34721d
Will Sugarman <will.sugarman@microsoft.com>
2020-01-11 22:31:07 -0800

    Add ResultPolicy.Default test

---
+++ src/Sweetener.Reliability.Test/Policies/Result/ResultPolicy.Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sweetener.Reliability.Test
+{
+    [TestClass]
+    public class ResultPolicyTest
+    {
+        [TestMethod]
+        public void Default()
+        {
+            ResultHandler<string> resultPolicy = ResultPolicy.Default<string>();
+            Assert.AreEqual(ResultKind.Successful, resultPolicy(null         ));
+            Assert.AreEqual(ResultKind.Successful, resultPolicy(string.Empty ));
+            Assert.AreEqual(ResultKind.Successful, resultPolicy("Hello World"));
+
+            // Only one instance
+            Assert.AreSame(ResultPolicy.Default<int>(), ResultPolicy.Default<int>());
+        }
+    }
+}
