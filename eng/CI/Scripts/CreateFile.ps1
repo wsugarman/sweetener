c4537e6754a1c20919de02771292f25a3cee2784
Will Sugarman <will.sugarman@microsoft.com>
2020-03-18 09:22:21 -0700

    Read Strong Name Key from Azure Key Vault (#43)
    - Read .snk file from Azure Key Vault with other secrets
    - Download content as a Base64 string and write it to a temporary local file for signing

---
+++ eng/CI/Scripts/CreateFile.ps1
@@ -0,0 +1,23 @@
+param
+(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Base64StringContent,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $OutputFilePath
+)
+
+# Turn off trace and stop on any error
+Set-PSDebug -Off
+$ErrorActionPreference = "Stop"
+
+if ([string]::IsNullOrWhiteSpace($Base64StringContent))
+{
+    throw [InvalidOperationException]::new("No file content specified")
+}
+
+# Extract the bytes and write them to the file
+$FileBytes = [System.Convert]::FromBase64String($Base64StringContent)
+[System.IO.File]::WriteAllBytes($OutputFilePath, $FileBytes)
