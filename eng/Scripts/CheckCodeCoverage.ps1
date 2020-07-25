8ea571684b22814ae897d9a9c8ea9feb1ca7a2b2
Will Sugarman <will.sugarman@microsoft.com>
2020-07-25 09:05:07 -0700

    Enforce Code Coverage Thresholds (#63)
    - Add CheckCodeCoverage.ps1 that throws an exception if the line or branch coverage does not meet a threshold
    - Run CheckCodeCoverage.ps1 in validation pipeline step
    - Update MSTest packages

---
+++ eng/Scripts/CheckCodeCoverage.ps1
@@ -0,0 +1,59 @@
+param
+(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $CodeCoverageFile,
+
+    [Parameter(Mandatory=$False)]
+    [Nullable[double]]
+    $LineCoverageThreshold = $null,
+
+    [Parameter(Mandatory=$False)]
+    [Nullable[double]]
+    $BranchCoverageThreshold = $null
+)
+
+# Turn off trace and stop on any error
+Set-PSDebug -Off
+$ErrorActionPreference = "Stop"
+
+# TODO: Pass thresholds to coverlet collector when supported
+[xml]$CodeCoverageXML = Get-Content $CodeCoverageFile
+
+# Line Coverage
+if ($LineCoverageThreshold)
+{
+    if (($LineCoverageThreshold -lt 0) -or ($LineCoverageThreshold -gt 1))
+    {
+        throw [ArgumentOutOfRangeException]::new("LineCoverageThreshold", $LineCoverageThreshold, "Line coverage threshold must be between 0 and 1")
+    }
+
+    $LineCoverage = Select-Xml -Xml $CodeCoverageXML -XPath "/coverage/@line-rate" | % {[double]::Parse($_.Node.Value)}
+    if ($LineCoverage -lt $LineCoverageThreshold)
+    {
+        throw "{0:P} line coverage failed to meet threshold {1:P}" -f $LineCoverage, $LineCoverageThreshold
+    }
+    else
+    {
+        Write-Host ("Passed with {0:P} line coverage!" -f $LineCoverage)
+    }
+}
+
+# Branch Coverage
+if ($BranchCoverageThreshold)
+{
+    if (($BranchCoverageThreshold -lt 0) -or ($BranchCoverageThreshold -gt 1))
+    {
+        throw [ArgumentOutOfRangeException]::new("BranchCoverageThreshold", $BranchCoverageThreshold, "Branch coverage threshold must be between 0 and 1")
+    }
+
+    $BranchCoverage = Select-Xml -Xml $CodeCoverageXML -XPath "/coverage/@branch-rate" | % {[double]::Parse($_.Node.Value)}
+    if ($BranchCoverage -lt $BranchCoverageThreshold)
+    {
+        throw "{0:P} branch coverage failed to meet threshold {1:P}" -f $BranchCoverage, $BranchCoverageThreshold
+    }
+    else
+    {
+        Write-Host ("Passed with {0:P} branch coverage!" -f $BranchCoverage)
+    }
+}
