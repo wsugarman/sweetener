cd774ee8d7730316879f27ef052f168fac88b857
Will Sugarman <will.sugarman@microsoft.com>
2021-04-04 10:44:12 -0700

    Add Enumerator.Empty and Update Folder Structure (#68)
    - Add Enumerator.Empty
    - Match folder structure and namespaces

---
+++ src/Sweetener/Linq/Enumerator.cs
@@ -0,0 +1,48 @@
+﻿// Copyright © William Sugarman.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sweetener.Linq
+{
+    /// <summary>
+    /// Provides a set of <see langword="static"/> methods for iterating over elements with an <see cref="IEnumerator{T}"/>.
+    /// </summary>
+    public static class Enumerator
+    {
+        /// <summary>
+        /// Returns an empty <see cref="IEnumerator{T}"/> that has the specified type argument.
+        /// </summary>
+        /// <typeparam name="T">The type to assign to the type parameter of the returned generic <see cref="IEnumerator{T}"/>.</typeparam>
+        /// <returns>An empty <see cref="IEnumerator{T}"/> whose type argument is <typeparamref name="T"/>.</returns>
+        public static IEnumerator<T> Empty<T>()
+            => EmptyEnumerator<T>.Value;
+
+        private sealed class EmptyEnumerator<T> : IEnumerator<T>
+        {
+            public static readonly EmptyEnumerator<T> Value = new EmptyEnumerator<T>();
+
+            private EmptyEnumerator()
+            { }
+
+            // Current is undefined if MoveNext() has not been called.
+            // For the sake of performance, we'll skip any state and simply throw.
+            public T Current => throw new NotSupportedException(SR.EmptyEnumeratorMessage);
+
+            object IEnumerator.Current => Current!;
+
+            // There is precedence for this behavior.
+            // The List<T>.Enumerator class can be disposed without affecting any of its operations.
+            public void Dispose()
+            { }
+
+            public bool MoveNext()
+                => false;
+
+            public void Reset()
+            { }
+        }
+    }
+}
--- src/Sweetener/SR.cs
+++ src/Sweetener/SR.cs
@@ -10,6 +10,8 @@ namespace Sweetener
     {
         public static string ArgumentNegativeMessage => ExceptionResourceManager.GetString(nameof(ArgumentNegativeMessage), CultureInfo.CurrentUICulture);
 
+        public static string EmptyEnumeratorMessage => ExceptionResourceManager.GetString(nameof(EmptyEnumeratorMessage), CultureInfo.CurrentUICulture);
+
         public static string UndefinedOptionalValueMessage => ExceptionResourceManager.GetString(nameof(UndefinedOptionalValueMessage), CultureInfo.CurrentUICulture);
 
         private static readonly ResourceManager ExceptionResourceManager = new ResourceManager("Sweetener.Resources.Exceptions", typeof(SR).Assembly);
