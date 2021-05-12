9586c372eac47b545fc6a8586e9e3eae286cd213
Will Sugarman <will.sugarman@microsoft.com>
2021-05-12 12:11:09 -0700

    Begin Addition of Configuration Extension Projects (#74)
    - Add three new projects to augment Microsoft's Configuration and Options extensions
       - Sweetener.Extensions.Configuration.Abstractions
       - Sweetener.Extensions.Configuration.Binder
       - Sweetener.Extensions.Options.ConfigurationExtensions
    - There projects operate on a new attribute called ConfigurationSectionAttribute which enables developers to indicate that a given data type represents a configuration section with a specified key. The binder library provides utilities for retrieving these sections automatically while the configuration extensions library provides utilities for adding them to the dependency injection service container.

---
+++ src/Sweetener.Extensions.Configuration.Abstractions.Test/ConfigurationSectionAttribute.Test.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sweetener.Extensions.Configuration.Test
+{
+    [TestClass]
+    public class ConfigurationSectionAttributeTest
+    {
+        [TestMethod]
+        public void Ctor()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new ConfigurationSectionAttribute(null!));
+
+            // Root
+            Assert.AreEqual(ConfigurationSectionKey.None, new ConfigurationSectionAttribute(ConfigurationSectionKey.None).Key);
+
+            // Non-root
+            Assert.AreEqual("Settings", new ConfigurationSectionAttribute("Settings").Key);
+        }
+
+        [TestMethod]
+        public void Annotation()
+        {
+            // Found
+            ConfigurationSectionAttribute? attribute = Attribute.GetCustomAttribute(
+                typeof(TestSettings),
+                typeof(ConfigurationSectionAttribute)) as ConfigurationSectionAttribute;
+
+            Assert.IsNotNull(attribute);
+            Assert.AreEqual("Testing", attribute.Key);
+
+            // Not Found (Cannot Inherit)
+            Assert.IsNull(Attribute.GetCustomAttribute(typeof(NewSettings), typeof(ConfigurationSectionAttribute)));
+        }
+
+        [ConfigurationSection("Testing")]
+        private class TestSettings
+        { }
+
+        [SuppressMessage("Performance", "CA1812: Avoid uninstantiated internal classes", Justification = "Type used in tests.")]
+        private sealed class NewSettings : TestSettings
+        { }
+    }
+}
---
+++ src/Sweetener.Extensions.Configuration.Abstractions/ConfigurationSectionAttribute.cs
@@ -0,0 +1,31 @@
+﻿// Copyright © William Sugarman.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Sweetener.Extensions.Configuration
+{
+    /// <summary>
+    /// Specifies that the attributed class represents a section in a configuration. This class cannot be inherited.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class ConfigurationSectionAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the key for this section.
+        /// </summary>
+        /// <remarks>
+        /// The key may or may not be found at the root of a configuration.
+        /// </remarks>
+        /// <value>The key for the attributed section's settings.</value>
+        public string Key { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationSectionAttribute"/>
+        /// class with the specified <paramref name="key"/>.
+        /// </summary>
+        /// <param name="key">The key for this section</param>
+        public ConfigurationSectionAttribute(string key)
+            => Key = key ?? throw new ArgumentNullException(nameof(key));
+    }
+}
