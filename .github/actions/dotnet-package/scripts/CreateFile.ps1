bf4226a5badc1206868a1a4b1843d39af38751c3
Will Sugarman <will.sugarman@microsoft.com>
2021-12-21 18:48:01 -0500

    Create dir if necessary

--- .github/actions/dotnet-package/scripts/CreateFile.ps1
+++ .github/actions/dotnet-package/scripts/CreateFile.ps1
@@ -18,6 +18,12 @@ if ([string]::IsNullOrWhiteSpace($Base64StringContent))
     throw [InvalidOperationException]::new("No file content specified")
 }
 
+if ([string]::IsNullOrWhiteSpace($OutputFilePath))
+{
+    throw [InvalidOperationException]::new("No output file specified")
+}
+
 # Extract the bytes and write them to the file
 $fileBytes = [System.Convert]::FromBase64String($Base64StringContent)
+New-Item -ItemType 'file' -Path $OutputFilePath -Force
 [System.IO.File]::WriteAllBytes($OutputFilePath, $fileBytes)
