7b5929a9900522677d28aa33bab0b62cbc80e996
Will Sugarman <will.sugarman@microsoft.com>
2022-05-27 17:51:36 -0700

    Add compare method

--- src/Core/Sweetener.Test/Collections/Generic/BinaryComparer.Test.cs
+++ src/Core/Sweetener.Test/Collections/Generic/BinaryComparer.Test.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sweetener.Collections.Generic;
 
@@ -18,12 +20,12 @@ public class BinaryComparerTest
         Assert.AreEqual(0, BinaryComparer.Instance.Compare(a, a));
 
         // Other test cases
-        Compare(BinaryComparer.Instance.Compare, treatNullEmpty: false);
+        Compare(BinaryComparer.Instance.Compare);
     }
 
     [TestMethod]
     public void Compare_ReadOnlySpan()
-        => Compare((x, y) => BinaryComparer.Compare(x, y), treatNullEmpty: true);
+        => Compare((x, y) => BinaryComparer.Compare(x, y), skipNullCase: true);
 
     [TestMethod]
     public void Compare_ReadOnlySpan_x86()
@@ -56,6 +58,41 @@ public class BinaryComparerTest
             });
 
     [TestMethod]
+    public void Compare_Stream()
+        => Compare(
+            (x, y) =>
+            {
+                using Stream? xStream = x != null ? new MemoryStream(x) : null;
+                using Stream? yStream = y != null ? new MemoryStream(y) : null;
+
+                return BinaryComparer.Instance.Compare(xStream, yStream);
+            });
+
+    [TestMethod]
+    public void Compare_Stream_x32()
+        => Compare(
+            (x, y) =>
+            {
+                using Stream xStream = new MemoryStream(x!);
+                using Stream yStream = new MemoryStream(y!);
+
+                return BinaryComparer.Compare32(xStream, yStream);
+            },
+            skipNullCase: true);
+
+    [TestMethod]
+    public void Compare_Stream_x64()
+        => Compare(
+            (x, y) =>
+            {
+                using Stream xStream = new MemoryStream(x!);
+                using Stream yStream = new MemoryStream(y!);
+
+                return BinaryComparer.Compare64(xStream, yStream);
+            },
+            skipNullCase: true);
+
+    [TestMethod]
     public void Equals_Array()
     {
         // Same reference
@@ -63,12 +100,12 @@ public class BinaryComparerTest
         Assert.IsTrue(BinaryComparer.Instance.Equals(a, a));
 
         // Other test cases
-        Equals(BinaryComparer.Instance.Equals, treatNullEmpty: false);
+        Equals(BinaryComparer.Instance.Equals);
     }
 
     [TestMethod]
     public void Equals_ReadOnlySpan()
-        => Equals((x, y) => BinaryComparer.Equals(x, y), treatNullEmpty: true);
+        => Equals((x, y) => BinaryComparer.Equals(x, y), skipNullCase: true);
 
     [TestMethod]
     public void Equals_ReadOnlySpan_x86()
@@ -102,24 +139,19 @@ public class BinaryComparerTest
 
     [TestMethod]
     public void GetHashCode_Array()
-        => Equals((x, y) => BinaryComparer.Instance.GetHashCode(x) == BinaryComparer.Instance.GetHashCode(y), treatNullEmpty: false);
+        => Equals((x, y) => BinaryComparer.Instance.GetHashCode(x) == BinaryComparer.Instance.GetHashCode(y));
 
     [TestMethod]
     public void GetHashCode_ReadOnlySpan()
-        => Equals((x, y) => BinaryComparer.GetHashCode(x) == BinaryComparer.GetHashCode(y), treatNullEmpty: true);
+        => Equals((x, y) => BinaryComparer.GetHashCode(x) == BinaryComparer.GetHashCode(y), skipNullCase: true);
 
-    private static void Compare(Func<byte[]?, byte[]?, int> compare, bool treatNullEmpty)
+    private static void Compare(Func<byte[]?, byte[]?, int> compare, bool skipNullCase = false)
     {
         // Null + Empty
-        Assert.AreEqual(0, compare(null, null));
+        if (!skipNullCase)
+        {
+            Assert.AreEqual(0, compare(null, null));
 
-        if (treatNullEmpty)
-        {
-            Assert.AreEqual(0, compare(null, Array.Empty<byte>()));
-            Assert.AreEqual(0, compare(Array.Empty<byte>(), null));
-        }
-        else
-        {
             Assert.IsTrue(compare(null, Array.Empty<byte>()) < 0);
             Assert.IsTrue(compare(Array.Empty<byte>(), null) > 0);
         }
@@ -127,6 +159,7 @@ public class BinaryComparerTest
         Compare(compare);
     }
 
+    [SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations", Justification = "Avoid reference equality.")]
     private static void Compare(Func<byte[], byte[], int> compare)
     {
         byte[] aligned1 = new byte[]
@@ -157,27 +190,33 @@ public class BinaryComparerTest
         };
 
         // Less Than
-        Assert.IsTrue(compare(aligned1, aligned2) < 0); // Same length
-        Assert.IsTrue(compare(extra1  , extra2  ) < 0); // Same length (unaligned)
-        Assert.IsTrue(compare(aligned1, extra1  ) < 0); // Different length
+        Assert.IsTrue(compare(aligned1           , aligned2) < 0); // Same length
+        Assert.IsTrue(compare(extra1             , extra2  ) < 0); // Same length (unaligned)
+        Assert.IsTrue(compare(aligned1           , extra1  ) < 0); // Different length
+        Assert.IsTrue(compare(Array.Empty<byte>(), aligned1) < 0); // Different length (empty)
 
         // Equal
-        Assert.AreEqual(0, compare(aligned1, (byte[])aligned1.Clone())); // Aligned
-        Assert.AreEqual(0, compare(extra1  , (byte[])extra1  .Clone())); // Unaligned
+        Assert.AreEqual(0, compare(aligned1   , (byte[])aligned1.Clone())); // Aligned
+        Assert.AreEqual(0, compare(extra1     , (byte[])extra1  .Clone())); // Unaligned
+        Assert.AreEqual(0, compare(new byte[0], new byte[0]             )); // Empty
 
         // Greater Than
-        Assert.IsTrue(compare(aligned2, aligned1) > 0); // Same length
-        Assert.IsTrue(compare(extra2  , extra1  ) > 0); // Same length (difference not in word)
-        Assert.IsTrue(compare(extra1  , aligned1) > 0); // Different length
+        Assert.IsTrue(compare(aligned2, aligned1           ) > 0); // Same length
+        Assert.IsTrue(compare(extra2  , extra1             ) > 0); // Same length (difference not in word)
+        Assert.IsTrue(compare(extra1  , aligned1           ) > 0); // Different length
+        Assert.IsTrue(compare(aligned1, Array.Empty<byte>()) > 0); // Different length (empty)
     }
 
-    private static void Equals(Func<byte[]?, byte[]?, bool> equals, bool treatNullEmpty)
+    private static void Equals(Func<byte[]?, byte[]?, bool> equals, bool skipNullCase = false)
     {
         // Null + Empty
-        Assert.IsTrue(equals(null, null));
+        if (!skipNullCase)
+        {
+            Assert.IsTrue(equals(null, null));
 
-        Assert.AreEqual(treatNullEmpty, equals(null, Array.Empty<byte>()));
-        Assert.AreEqual(treatNullEmpty, equals(Array.Empty<byte>(), null));
+            Assert.IsFalse(equals(null, Array.Empty<byte>()));
+            Assert.IsFalse(equals(Array.Empty<byte>(), null));
+        }
 
         // Different lengths
         Assert.IsFalse(equals(new byte[] { 1, 2, 3 }, new byte[] { 4, 5 }   ));
@@ -228,5 +267,12 @@ public class BinaryComparerTest
 
         Assert.IsFalse(equals(extra1, extra2));
         Assert.IsFalse(equals(extra2, extra1));
+    }
+
+    private enum NullTesting
+    {
+        Skip,
+        Include,
+        SameAsEmpty,
     }
 }
--- src/Core/Sweetener/Collections/Generic/BinaryComparer.cs
+++ src/Core/Sweetener/Collections/Generic/BinaryComparer.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Hashing;
+using System.Runtime.InteropServices;
 
 namespace Sweetener.Collections.Generic;
 
@@ -15,7 +18,7 @@ namespace Sweetener.Collections.Generic;
 /// <see cref="IComparer{T}"/> and <see cref="IEqualityComparer{T}"/>. Instead, the comparer exposes
 /// <see langword="static"/> methods for operating upon <see cref="ReadOnlySpan{T}"/> structures.
 /// </remarks>
-public sealed class BinaryComparer : IComparer<byte[]?>, IEqualityComparer<byte[]?>
+public sealed class BinaryComparer : IComparer<byte[]?>, IComparer<Stream?>, IEqualityComparer<byte[]?>
 {
     /// <summary>
     /// Gets the singleton <see cref="BinaryComparer"/> instance for comparing byte arrays.
@@ -76,6 +79,58 @@ public sealed class BinaryComparer : IComparer<byte[]?>, IEqualityComparer<byte[
             return 1;
 
         return Compare(new ReadOnlySpan<byte>(x), new ReadOnlySpan<byte>(y));
+    }
+
+    /// <summary>
+    /// Compares two streams and returns a value indicating whether one is less than, equal to, or greater than the other.
+    /// </summary>
+    /// <remarks>
+    /// Streams are first compared by their byte values, starting from their current position, followed by their
+    /// respective lengths such that shorter streams are considered smaller values.
+    /// </remarks>
+    /// <param name="x">The first stream to compare.</param>
+    /// <param name="y">The second stream to compare.</param>
+    /// <returns>
+    /// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>,
+    /// as shown in the following table.
+    /// <list type="table">
+    /// <listheader>
+    /// <term>Return Value</term>
+    /// <description>Description</description>
+    /// </listheader>
+    /// <item>
+    /// <term>Less than zero</term>
+    /// <description>
+    /// <paramref name="x"/> is less than <paramref name="y"/>, or <paramref name="x"/> is <see langword="null"/>
+    /// and <paramref name="y"/> is not <see langword="null"/>.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>Zero</term>
+    /// <description>
+    /// <paramref name="x"/> is equal to <paramref name="y"/>, or <paramref name="x"/> and <paramref name="y"/>
+    /// are both <see langword="null"/>.
+    /// </description>
+    /// </item>
+    /// <item>
+    /// <term>Greater than zero</term>
+    /// <description>
+    /// <paramref name="x"/> is greater than <paramref name="y"/>, or <paramref name="y"/> is <see langword="null"/>
+    /// and <paramref name="x"/> is not <see langword="null"/>.
+    /// </description>
+    /// </item>
+    /// </list>
+    /// </returns>
+    public int Compare(Stream? x, Stream? y)
+    {
+        if (ReferenceEquals(x, y))
+            return 0;
+        else if (x is null)
+            return -1;
+        else if (y is null)
+            return 1;
+
+        return IntPtr.Size == sizeof(uint) ? Compare32(x, y) : Compare64(x, y);
     }
 
     /// <summary>
@@ -264,6 +319,148 @@ public sealed class BinaryComparer : IComparer<byte[]?>, IEqualityComparer<byte[
         return Compare((byte*)x, (byte*)y, xCount, yCount);
     }
 
+#if NETCOREAPP2_1_OR_GREATER
+    internal static int Compare32(Stream x, Stream y)
+    {
+        unsafe
+        {
+            ulong xLong;
+            ulong yLong;
+            Span<byte> xBuffer = new Span<byte>(&xLong, 8);
+            Span<byte> yBuffer = new Span<byte>(&yLong, 8);
+
+            int cmp;
+            int xCount;
+            int yCount;
+
+            do
+            {
+                xCount = FillBuffer(x, xBuffer);
+                yCount = FillBuffer(y, yBuffer);
+
+                if (xCount < sizeof(ulong) || yCount < sizeof(ulong))
+                    break;
+
+                cmp = xLong.CompareTo(yLong);
+                if (cmp != 0)
+                    return cmp;
+            } while (true);
+
+            return Compare((byte*)&xLong, (byte*)&yLong, xCount, yCount);
+        }
+    }
+
+    internal static int Compare64(Stream x, Stream y)
+    {
+        unsafe
+        {
+            ulong xLong;
+            ulong yLong;
+            Span<byte> xBuffer = new Span<byte>(&xLong, 8);
+            Span<byte> yBuffer = new Span<byte>(&yLong, 8);
+
+            int cmp;
+            int xCount;
+            int yCount;
+
+            do
+            {
+                xCount = FillBuffer(x, xBuffer);
+                yCount = FillBuffer(y, yBuffer);
+
+                if (xCount < sizeof(ulong) || yCount < sizeof(ulong))
+                    break;
+
+                cmp = xLong.CompareTo(yLong);
+                if (cmp != 0)
+                    return cmp;
+            } while (true);
+
+            return Compare((byte*)&xLong, (byte*)&yLong, xCount, yCount);
+        }
+    }
+#else
+    internal static int Compare32(Stream x, Stream y)
+    {
+        unsafe
+        {
+            byte[] xBuffer = ArrayPool<byte>.Shared.Rent(sizeof(uint));
+            byte[] yBuffer = ArrayPool<byte>.Shared.Rent(sizeof(uint));
+
+            try
+            {
+                fixed (byte* xPtr = xBuffer)
+                fixed (byte* yPtr = yBuffer)
+                {
+                    int cmp;
+                    int xCount;
+                    int yCount;
+
+                    do
+                    {
+                        xCount = FillBuffer(x, xBuffer, sizeof(uint));
+                        yCount = FillBuffer(y, yBuffer, sizeof(uint));
+
+                        if (xCount < sizeof(uint) || yCount < sizeof(uint))
+                            break;
+
+                        cmp = ((uint*)xPtr)->CompareTo(*(uint*)yPtr);
+                        if (cmp != 0)
+                            return cmp;
+                    } while (true);
+
+                    return Compare(xPtr, yPtr, xCount, yCount);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(xBuffer);
+                ArrayPool<byte>.Shared.Return(yBuffer);
+            }
+        }
+    }
+
+    internal static int Compare64(Stream x, Stream y)
+    {
+        unsafe
+        {
+            byte[] xBuffer = ArrayPool<byte>.Shared.Rent(sizeof(ulong));
+            byte[] yBuffer = ArrayPool<byte>.Shared.Rent(sizeof(ulong));
+
+            try
+            {
+                fixed (byte* xPtr = xBuffer)
+                fixed (byte* yPtr = yBuffer)
+                {
+                    int cmp;
+                    int xCount;
+                    int yCount;
+
+                    do
+                    {
+                        xCount = FillBuffer(x, xBuffer, sizeof(ulong));
+                        yCount = FillBuffer(y, yBuffer, sizeof(ulong));
+
+                        if (xCount < sizeof(ulong) || yCount < sizeof(ulong))
+                            break;
+
+                        cmp = ((ulong*)xPtr)->CompareTo(*(ulong*)yPtr);
+                        if (cmp != 0)
+                            return cmp;
+                    } while (true);
+
+                    return Compare(xPtr, yPtr, xCount, yCount);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(xBuffer);
+                ArrayPool<byte>.Shared.Return(yBuffer);
+            }
+        }
+    }
+#endif
+
     internal static unsafe bool Equals(uint* x, uint* y, int count)
     {
         while (count >= sizeof(uint))
@@ -318,4 +515,37 @@ public sealed class BinaryComparer : IComparer<byte[]?>, IEqualityComparer<byte[
 
         return true;
     }
+
+#if NETCOREAPP2_1_OR_GREATER
+    private static int FillBuffer(Stream stream, Span<byte> buffer)
+    {
+        int read = 0;
+        while (read < buffer.Length)
+        {
+            int next = stream.Read(buffer);
+            if (next == 0)
+                break;
+
+            read += next;
+            buffer = buffer[read..];
+        }
+
+        return read;
+    }
+#else
+    private static int FillBuffer(Stream stream, byte[] buffer, int length)
+    {
+        int read = 0;
+        while (read < length)
+        {
+            int next = stream.Read(buffer, read, length - read);
+            if (next == 0)
+                break;
+
+            read += next;
+        }
+
+        return read;
+    }
+#endif
 }
